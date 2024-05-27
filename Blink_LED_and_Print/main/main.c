0173e7430ae14fbdbc9a3b550a820b53f3d72fe0
Steven Bennett <benthamsoftwareservices@gmail.com>
2024-05-27 17:42:02 +0100

    Updates

--- Blink_LED_and_Print/main/main.c
+++ Blink_LED_and_Print/main/main.c
@@ -1,19 +1,19 @@
 #include "driver/gpio.h"
 #include "esp_event.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/unistd.h>
 #include <unistd.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
-//Led is connected GPIO 2, the on-board LED on an ESP32 module
+// Led is connected GPIO 2, the on-board LED on an ESP32 module
 #define led 2
 
-int count = 0; //A value to be incremented and compared to predefined_value
+int count = 0; // A value to be incremented and compared to predefined_value
 int predefined_value = 1000; // Define the predefined value
-int var = 0; // Initialise the variable
+int var = 0;                 // Initialise the variable
 
 void app_main(void) {
 
@@ -21,27 +21,27 @@ void app_main(void) {
   gpio_set_direction(led,
                      GPIO_MODE_OUTPUT); // led pin initialise as an output //
 
-  int level = 1; //led OFF value
+  int level = 1; // led OFF value
 
   while (true) {
     printf("Hello from app_main!\n");
-    //sleep(1);
-    vTaskDelay(200 / portTICK_PERIOD_MS);
-
+    // sleep(1); //Delay 1 second
+    vTaskDelay(200 / portTICK_PERIOD_MS); //Delay 200mS
 
     if (count == predefined_value) {
-            printf("Predefined value reached. Run While loop...\n");
-            while (var < 10) {
-				printf("Running While loop...\n");
-				sleep(1);
+      printf("Predefined value reached. Run While loop...\n");
+      while (var < 10) {
+        printf("Running While loop...\n");
+        sleep(1); //Delay 1 second
         // Do nothing inside the loop
-    }
+      }
     }
     count++;
     printf("The value of count is: %d\n", count);
-    //sleep(1);
-    vTaskDelay(200 / portTICK_PERIOD_MS);
-    gpio_set_level(led, level); // led will be turn ON (level = 0) and OFF (level =1) as value of level
-    level = !level; // change the level value
+    // sleep(1); //Delay 1 second
+    vTaskDelay(200 / portTICK_PERIOD_MS); //Delay 200mS
+    gpio_set_level(led, level); // led will be turn ON (level = 0) and OFF
+                                // (level =1) as value of level
+    level = !level;             // change the level value
   }
 }
