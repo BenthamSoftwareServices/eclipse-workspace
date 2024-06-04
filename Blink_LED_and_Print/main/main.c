139a7523e76136f3056d10c9483b95b6a33497ee
Steven Bennett <benthamsoftwareservices@gmail.com>
2024-06-04 15:56:20 +0100

    updates

--- Blink_LED_and_Print/main/main.c
+++ Blink_LED_and_Print/main/main.c
@@ -17,31 +17,30 @@ int var = 0;                 // Initialise the variable
 
 void app_main(void) {
 
-  esp_rom_gpio_pad_select_gpio(led); // Initialise the led pin (gpio 2) //
-  gpio_set_direction(led,
-                     GPIO_MODE_OUTPUT); // led pin initialise as an output //
+	esp_rom_gpio_pad_select_gpio(led); // Initialise the led pin (gpio 2) //
+	gpio_set_direction(led, GPIO_MODE_OUTPUT); // led pin initialise as an output //
 
-  int level = 1; // led OFF value
+	int level = 1; // led OFF value
 
-  while (true) {
-    printf("Hello from app_main!\n");
-    // sleep(1); //Delay 1 second
-    vTaskDelay(200 / portTICK_PERIOD_MS); //Delay 200mS
+	while (true) {
+		printf("Hello from app_main!\n");
+		// sleep(1); //Delay 1 second
+		vTaskDelay(200 / portTICK_PERIOD_MS); //Delay 200mS
 
-    if (count == predefined_value) {
-      printf("Predefined value reached. Run While loop...\n");
-      while (var < 10) {
-        printf("Running While loop...\n");
-        sleep(1); //Delay 1 second
-        // Do nothing inside the loop
-      }
-    }
-    count++;
-    printf("The value of count is: %d\n", count);
-    // sleep(1); //Delay 1 second
-    vTaskDelay(200 / portTICK_PERIOD_MS); //Delay 200mS
-    gpio_set_level(led, level); // led will be turn ON (level = 0) and OFF
-                                // (level =1) as value of level
-    level = !level;             // change the level value
-  }
+		if (count == predefined_value) {
+			printf("Predefined value reached. Run While loop...\n");
+			while (var < 10) {
+				printf("Running While loop...\n");
+				sleep(1); //Delay 1 second
+				// Do nothing inside the loop
+			}
+		}
+		count++;
+		printf("The value of count is: %d\n", count);
+		// sleep(1); //Delay 1 second
+		vTaskDelay(200 / portTICK_PERIOD_MS); //Delay 200mS
+		gpio_set_level(led, level); // led will be turn ON (level = 0) and OFF
+									// (level =1) as value of level
+		level = !level;             // change the level value
+	}
 }
